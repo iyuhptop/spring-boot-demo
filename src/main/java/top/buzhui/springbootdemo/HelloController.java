1afd17e5548d0b4a5b13046ff529ef36ebacdbe8
haiping <haiping.yu@zoom.us>
2021-08-30 10:19:42 +0800

    feat: pom.xml update

--- src/main/java/top/buzhui/springbootdemo/HelloController.java
+++ src/main/java/top/buzhui/springbootdemo/HelloController.java
@@ -15,18 +15,18 @@ import java.util.Random;
 @RequestMapping
 public class HelloController {
 
-    private static List<String> poetry = new ArrayList<>();
-    private static Random random = new Random(System.currentTimeMillis());
+    private static final List<String> POETRY = new ArrayList<>();
+    private static final Random RANDOM = new Random(System.currentTimeMillis());
 
     static {
-        poetry.add("醉里不知天在水  满船清梦压星河");
-        poetry.add("弃我去者，昨日之日不可留  乱我心者，今日之日多烦忧");
-        poetry.add("北国风光，千里冰封，万里雪飘。望长城内外，惟余莽莽；大河上下，顿失滔滔");
+        POETRY.add("醉里不知天在水  满船清梦压星河");
+        POETRY.add("弃我去者，昨日之日不可留  乱我心者，今日之日多烦忧");
+        POETRY.add("北国风光，千里冰封，万里雪飘。望长城内外，惟余莽莽；大河上下，顿失滔滔");
     }
 
     @RequestMapping("/hello")
     public Object hello() {
-        return poetry.get(random.nextInt(3));
+        return POETRY.get(RANDOM.nextInt(3));
     }
 
     @RequestMapping("/_health")
